e136aad8a11536d3df0b2c8c32775ea5dec41c5b
Kamal Swarnkar <swarnkar889@gmail.com>
2026-04-02 17:42:13 +0530

    Phase 7: JWT authentication system implemented

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
 from .serializers import RegisterSerializer
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
 # Create your views here.
 class RegisterView(APIView):
@@ -14,3 +16,20 @@ class RegisterView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            refresh = RefreshToken.for_user(user)
+
+            return Response({
+                "access": str(refresh.access_token),
+                "refresh": str(refresh),
+            })
+        
+        return Response({"error" : "Invalid Credentials"}, status=400)
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,6 +75,17 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME' : timedelta(days=1),
+    'AUTH_HEADER_TYPES' : ('Bearer'),
+}
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
