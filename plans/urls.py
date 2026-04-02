d88af34c92b92180ab69e7f083d0bf21b0dee862
Kamal Swarnkar <swarnkar889@gmail.com>
2026-04-02 14:55:20 +0530

    Phase 6: Plan retrieval and today workout APIs

--- plans/urls.py
+++ plans/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import GeneratePlanView
+from .views import GeneratePlanView, CurrentPlanView, TodayWorkoutView
 
 urlpatterns = [
     path('generate/', GeneratePlanView.as_view()),
+    path('current/', CurrentPlanView.as_view()),
+    path('today', TodayWorkoutView.as_view()),
 ]
--- plans/views.py
+++ plans/views.py
@@ -26,3 +26,40 @@ class GeneratePlanView(APIView):
         serializer = WorkoutPlanSerializer(plan)
         
         return Response(serializer.data)
+
+class CurrentPlanView(APIView):
+    def get(self, request):
+        user = request.user
+
+        plan = WorkoutPlan.objects.filter(user=user).order_by('-week_start').first()
+
+        if not plan:
+            return Response({"message" : "Plan not found"})
+        
+        serializer = WorkoutPlanSerializer(plan)
+
+        return Response(serializer.data)
+    
+class TodayWorkoutView(APIView):
+    def get(self, request):
+        user = request.user
+
+        plan = WorkoutPlan.objects.filter(user=user).order_by('-week_start').first()
+
+        if not plan:
+            return Response({"message" : "Plan not found"})
+        
+        today_index = (date.today - plan.week_start).days + 1
+        workout_day = WorkoutDay.objects.filter(plan=plan, day_number=today_index).first()
+
+        if not workout_day:
+            return Response({"message" : "Rest Day!!"})
+        
+        data = {
+            "day" : workout_day.day_number,
+            "focus" : workout_day.focus,
+            "exercises" : workout_day.exercises
+        }
+
+        return Response(data)
+    
